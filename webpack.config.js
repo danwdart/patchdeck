c0afe70c56ef6d2b5aabf5008abf9281a68e2727
Dan <git@dandart.co.uk>
2017-04-16 22:04:38 +0100

    bootstrap 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,45 @@
+const webpack = require('webpack');
+
 module.exports = {
-   context: __dirname+'/js',
-   entry: './src/index.js',
-   output: {
-       path: './js',
-       publicPath: '/js/',
-       filename: "index.js"
-   },
-   module: {
-       loaders: [
-           { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-           { test: /\.css$/, loader: "style-loader!css-loader" },
-           { test: /\.woff2?/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-           { test: /\.ttf/, loader: "file-loader" },
-           { test: /\.eot/, loader: "file-loader" },
-           { test: /\.svg/, loader: "file-loader" },
-           { test: /\.glsl/, loader: "webpack-glsl-loader" }
-       ]
-   }
+    context: __dirname+'/js',
+    entry: ['bootstrap-loader', './src/index.js'],
+    output: {
+        path: __dirname+'./js',
+        publicPath: '/js/',
+        filename: "index.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /\.woff2?/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+            { test: /\.ttf/, loader: "file-loader" },
+            { test: /\.eot/, loader: "file-loader" },
+            { test: /\.svg/, loader: "file-loader" },
+            { test: /\.glsl/, loader: "webpack-glsl-loader" },
+            { test: /bootstrap[\/\\]dist[\/\\]js[\/\\]umd[\/\\]/, loader: 'imports-loader?jQuery=jquery' },
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin(
+            {
+                $: "jquery",
+                jQuery: "jquery",
+                "window.jQuery": "jquery",
+                Tether: "tether",
+                "window.Tether": "tether",
+                Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
+                Button: "exports-loader?Button!bootstrap/js/dist/button",
+                Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
+                Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
+                Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+                Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+                Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
+                Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
+                Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
+                Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+                Util: "exports-loader?Util!bootstrap/js/dist/util",
+            }
+        )
+    ]
 };
